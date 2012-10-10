70d00de0451eba2458b1196d558fa969f5e3d453
Yoji SHIDARA <dara@shidara.net>
2012-10-10 16:04:44 +0900

    Show a list of domain names on /

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,23 @@
+var awssum = require('awssum');
+var CloudSearch = awssum.load('amazon/cloudsearch').CloudSearch;
+
+CloudSearch.prototype.host = function() {
+   return 'localhost';
+};
+
+var cs = new CloudSearch({
+   accessKeyId: 'dummy-access-key-id',
+   secretAccessKey: 'dummy-access-key',
+});
+
+cs.addExtras = function(options, args) {
+   options.protocol = 'http';
+   options.port = 7575;
+};
+
 exports.index = function(req, res){
-  res.render('index');
+  cs.DescribeDomains(function(error, data) {
+    var domains = data.Body.DescribeDomainsResponse.DescribeDomainsResult.DomainStatusList.member;
+    res.render('index', {domains: domains});
+  })
 };
