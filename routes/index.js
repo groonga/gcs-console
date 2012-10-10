d6d0df524ac89602c68ccc9ba4eb29a3329e0b69
Yoji SHIDARA <dara@shidara.net>
2012-10-10 16:07:40 +0900

    Extract CloudSearch object to lib/cloudsearch.js

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,4 @@
-var awssum = require('awssum');
-var CloudSearch = awssum.load('amazon/cloudsearch').CloudSearch;
-
-CloudSearch.prototype.host = function() {
-   return 'localhost';
-};
-
-var cs = new CloudSearch({
-   accessKeyId: 'dummy-access-key-id',
-   secretAccessKey: 'dummy-access-key',
-});
-
-cs.addExtras = function(options, args) {
-   options.protocol = 'http';
-   options.port = 7575;
-};
+var cs = require('../lib/cloudsearch').cs;
 
 exports.index = function(req, res){
   cs.DescribeDomains(function(error, data) {
